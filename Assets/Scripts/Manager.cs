3fc7ad7ab0527fa1c76fe97aa0a9860c78de7bbe
Jing <273451@via.dk>
2021-05-04 23:04:59 +0800

    GreedyPiggy

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+
+    public List<cutepig> cuties;
+    public List<badpig> badPigs;
+
+
+    //
+    public static Manager _instance;  //use singleton pattern to help cutepigs "fly" and disappear one by one in order
+
+    private Vector3 originalPos; //original position of cute pig
+
+    private void Awake()
+    {
+        _instance = this;
+        if (cuties.Count > 0)
+        {
+            originalPos = cuties[0].transform.position;
+        }
+    }
+
+
+    private void Start()
+    {
+        Initiated();
+    }
+    //cutepig
+    private void Initiated()
+    {
+        for(int i =0; i< cuties.Count;i++)
+        {
+            if(i == 0)
+            {
+                cuties[i].transform.position = originalPos;
+                cuties[i].enabled = true;
+                cuties[i].springJoint2D.enabled = true;
+            }
+            else
+            {
+                cuties[i].enabled = false;
+                cuties[i].springJoint2D.enabled = false;
+            }
+        }
+    }
+
+
+
+    //see if cute pigs already won
+    public void NextCuty()
+    {
+        if(badPigs.Count > 0)
+        {
+            if(cuties.Count > 0)
+            {
+                //make next cutypig able to fly
+                Initiated();
+            }
+            else
+            {
+                //we lose the game
+
+            }
+        }
+        else
+        {
+            //we win,yeahhh! >.<
+        }
+    }
+}
---
+++ Assets/Scripts/cutepig.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cutepig : MonoBehaviour
+{
+    private bool isClicked = false;
+    public Transform rightPosition;
+    public float maxDistance = 2;
+
+    [HideInInspector]
+    public SpringJoint2D springJoint2D;
+    private Rigidbody2D rigidBody2D;
+
+    //lines
+    public LineRenderer rightLine;
+    public LineRenderer leftLine;
+    public Transform rightPos;
+    public Transform leftPos;
+
+
+    //
+    public GameObject boom;
+
+    private void Awake()
+    {
+        springJoint2D = GetComponent<SpringJoint2D>();
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnMouseDown() //press the mouse
+    {
+        isClicked = true;
+        rigidBody2D.isKinematic = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isClicked = false;
+        rigidBody2D.isKinematic = false;
+        Invoke("Fly", 0.1f);
+        //unable line
+        rightLine.enabled = false;
+        leftLine.enabled = false;
+    }
+
+    private void Update() //
+    {
+        if (isClicked)//if mouse keeps pressed,follow the positions
+        {
+            transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //transform.position += new Vector3(0,0,10) ??
+            transform.position += new Vector3(0, 0, -Camera.main.transform.position.z);
+
+            //if ...true, limit the distance
+            if (Vector3.Distance(transform.position, rightPosition.position) > maxDistance)
+            {
+                Vector3 pos = (transform.position - rightPosition.position).normalized;
+                //length
+                pos *= maxDistance; // max distance (vector)
+                transform.position = pos + rightPosition.position;
+            }
+            HandleLines();
+        }
+    }
+
+    void Fly()
+    {
+        springJoint2D.enabled = false; //let springjoint2d disabled -> let the pig fly
+        Invoke("HandleNextPigFly", 4); //wait 4s and ...
+    }
+
+    //draw line
+    void HandleLines()
+    {
+        rightLine.enabled = true;
+        leftLine.enabled = true;
+
+        rightLine.SetPosition(0, rightPos.position);
+        rightLine.SetPosition(1,transform.position);
+
+
+        leftLine.SetPosition(0, leftPos.position);
+        leftLine.SetPosition(1, transform.position);
+    }
+
+
+    void HandleNextPigFly()
+    {
+        Manager._instance.cuties.Remove(this);
+        Destroy(gameObject);
+        Instantiate(boom, transform.position, Quaternion.identity);
+        Manager._instance.NextCuty();
+    }
+}
